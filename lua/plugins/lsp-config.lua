744f3f75d02f6277c55c0812d31cab791e1c8844
Diogo Matos <alvesdiogo.mts@gmail.com>
2025-07-14 16:53:08 -0300

    diagnostics open list

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,6 +22,8 @@ return {
 			-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 
 			-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+			vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float,
+				{ noremap = true, silent = true })
 			-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 			vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
