cc62e344e9c235ebc86c8024a61f5fca51023b28
Diogo Matos <alvesdiogo.mts@gmail.com>
2025-07-16 10:55:38 -0300

    nvim-tree keymap para focus e toggle

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,4 +1,9 @@
 return {
   'nvim-tree/nvim-tree.lua',
   opts = {},
+  config = function ()
+    vim.keymap.set("n", "<leader>we", "<cmd>NvimTreeFocus<cr>", { desc = "Tree Open"})
+    vim.keymap.set("n", "<leader>wt", "<cmd>NvimTreeToggle<cr>", { desc = "Tree Toggle" })
+    require("nvim-tree").setup({})
+  end
 }
