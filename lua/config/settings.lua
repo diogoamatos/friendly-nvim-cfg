02110894afd2671f8b3a2f8efdd895b888fcae0e
Diogo Matos <alvesdiogo.mts@gmail.com>
2025-07-17 14:20:20 -0300

    ajusta essa parada

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -43,6 +43,10 @@ vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 vim.o.completeopt = "menuone,noselect"
 
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.confirm = true
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-    -- Theme inspired by Atom
-    -- 'navarasu/onedark.nvim',
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'tokyonight'
-    end,
+  -- Theme inspired by Atom
+  -- 'navarasu/onedark.nvim',
+  'folke/tokyonight.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'tokyonight'
+  end,
 }
