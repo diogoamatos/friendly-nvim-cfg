1eff3fa1570d14fd97e5b13fc19e2436bff82527
Diogo Matos <alvesdiogo.mts@gmail.com>
2025-07-22 10:36:15 -0300

    Adicionando scrollEOF.nvim deixa no meio para editar

--- lua/plugins/plugins-util.lua
+++ lua/plugins/plugins-util.lua
@@ -9,5 +9,13 @@ return {
 	{ 'folke/which-key.nvim',     opts = {} },
 
 	{ 'numToStr/Comment.nvim',    opts = {} },
+	-- simple and lightweight plugin to make scrolloff go past the end of the files
+	{
+		'Aasim-A/scrollEOF.nvim',
+		event = { 'CursorMoved', 'WinScrolled' },
+		opts = {
+			insert_mode = true
+		},
+	},
 
 }
