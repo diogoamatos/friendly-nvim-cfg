b56b8ea1ad9128f5de8a1edd3eb35076e0836662
Diogo Matos <alvesdiogo.mts@gmail.com>
2025-08-05 10:07:21 -0300

    lualine: colorscheme github-nvim-theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,14 @@
 return {
-  -- Set lualine as statusline
-  'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = 'tokyonight',
-      component_separators = '|',
-      section_separators = '',
-    },
-  },
+	-- Set lualine as statusline
+	"nvim-lualine/lualine.nvim",
+	-- See `:help lualine.txt`
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "github_dark_default",
+			component_separators = "|",
+			section_separators = "",
+		},
+	},
 }
